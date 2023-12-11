94a6254135fdb0e6320fd2767940f295b6681ec0
Nikodem Korohoda <nikodemko@interia.pl>
2023-12-11 21:00:49 +0100

    Flask -> FastAPI + bit of refactor

--- python/fakeClient.py
+++ python/fakeClient.py
@@ -3,9 +3,13 @@ import json
 import requests
 
 url = 'http://localhost:9091/optimization'
-json_data = json.dumps(json.load(open("../input_metadata/data_kijowska_new.json")))
-headers = {'Content-Type': 'application/json'}
 
-response = requests.post(url, data=json_data, headers=headers)
-print(response)
-print(response.content)
+json_data = json.load(open("../input_metadata/data_example_newer.json"))
+
+response = requests.get('http://localhost:9091/')
+print(response.json())
+
+dict_ = {"optimization_request": json_data}
+
+response = requests.post(f"{url}", json=dict_)
+print(response.json())
