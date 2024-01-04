067666667fbe78ce125086426f20dcca7ba68fb5
Nikodem Korohoda <nikodemko@interia.pl>
2024-01-04 22:11:11 +0100

    last fixes

--- README.md
+++ README.md
@@ -7,9 +7,18 @@ Wykorzystuje on program napisany w języku MiniZinc, z wykorzystaniem solvera [C
 
 ## Jak uruchomić moduł
 
+Optymalizator można uruchomić na dwa sposoby.
+Zalecanym sposobem jest uruchomienie lokalnie, jednak w razie problemów możliwe jest również wykorzystanie Dockera
+
+### Lokalnie
+
 * katalog projektu należy otworzyć w ulubionym IDE (przykładowo PyCharm)
 * uruchomić [aplikację](python/Server.py) z konfiguracją używającą zmiennych z pliku [.env](.env) (w Idea można użyć do tego [plugin](https://plugins.jetbrains.com/plugin/7861-envfile)) 
 
+### Za pomocą Dockera
+
+* uruchomić Dockera
+* w katalogu projektu: `docker compose up`
 
 ## Dokumentacja
 
--- python/Optimizer.py
+++ python/Optimizer.py
@@ -1,3 +1,4 @@
+import os
 from datetime import timedelta
 from glob import glob
 
@@ -12,13 +13,20 @@ class Optimizer:
         """Creates Optimizer class with MiniZinc model and Minizinc data files on given paths"""
         self.model = Model(model_path)
         self.model_for_comparison = Model(model_model_for_comparison_path)
+        self.DEFAULT_SOLVER = os.getenv('DEFAULT_SOLVER')
 
-    def solve(self, optimization_request: OptimizationRequest, solver: str):
+    def solve(self, optimization_request: OptimizationRequest, solver_name: str):
         """Solves problem and returns best result"""
         minizinc_data_path = f'../minizinc/data/{optimization_request.idx}.dzn'
 
         self.model.add_file(minizinc_data_path)
-        instance = Instance(Solver.lookup(solver, refresh=True), self.model)
+
+        try:
+            solver = Solver.lookup(solver_name, refresh=True)
+        except LookupError:
+            solver = Solver.lookup(self.DEFAULT_SOLVER, refresh=True)
+
+        instance = Instance(solver, self.model)
 
         result = instance.solve(timeout=timedelta(seconds=optimization_request.optimization_time),
                                 optimisation_level=2, intermediate_solutions=True)
@@ -33,13 +41,13 @@ class Optimizer:
 
         return parse_solver_result(result.solution[-1].is_light_on, optimization_request.scaling)
 
-    def calculate_previous_objective(self, optimization_request: OptimizationRequest, solver: str):
+    def calculate_previous_objective(self, optimization_request: OptimizationRequest, solver: Solver):
         minizinc_data_path = glob(f'../minizinc/data/{optimization_request.idx}_for_comparison.dzn')
         if len(minizinc_data_path) == 0:
             return 0
 
         self.model_for_comparison.add_file(minizinc_data_path[0])
-        instance = Instance(Solver.lookup(solver, refresh=True), self.model_for_comparison)
+        instance = Instance(solver, self.model_for_comparison)
 
         result = instance.solve(optimisation_level=2)
         return result.objective / optimization_request.scaling
